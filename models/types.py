346986b9f83f65a556e7f3cf8beb6b9c3e7b6919
sharkspeare <jack04309487@gmail.com>
2026-04-25 23:52:28 +0800

    refactor: 將types內的資料結構用TypeDict規範

--- models/types.py
+++ models/types.py
@@ -1,4 +1,59 @@
-EntryRaw =  dict[str, str]
+from typing import TypedDict, NotRequired
+
+class EntryRaw(TypedDict):
+
+    #頭尾時間
+    StartTime: str
+    EndTime: str
+
+    #溫度
+    Temperature: NotRequired[str]
+
+    #降雨機率
+    ProbabilityOfPrecipitation: NotRequired[str]
+
+    #風向
+    WindDirection: NotRequired[str]
+
+    #風速
+    WindSpeed: NotRequired[str]
+
+    #紫外線
+    UVIndex: NotRequired[str]
+    UVExposureLevel: NotRequired[str]
+
+    #天氣現象
+    Weather: NotRequired[str]
+
+    #綜合描述
+    WeatherDescription: NotRequired[str]
+
 TimeRaw =  list[EntryRaw]
-ElementRaw = dict[str, str|TimeRaw]
-LocationRaw = dict[str, str|ElementRaw]
+
+class ElementRaw(TypedDict):
+    ElementName: str
+    Time: TimeRaw
+
+class LocationRaw(TypedDict):
+    LocationName: str
+    
+    #溫度
+    Temperature: ElementRaw
+
+    #降雨機率
+    ProbabilityOfPrecipitation: ElementRaw
+
+    #風向
+    WindDirection: ElementRaw
+
+    #風速
+    WindSpeed: ElementRaw
+
+    #紫外線
+    UVIndex: ElementRaw
+
+    #天氣現象
+    Weather: ElementRaw
+
+    #綜合描述
+    WeatherDescription: ElementRaw

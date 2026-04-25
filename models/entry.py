08de9e00e777ffce6bf3674d969b5107d8dd6558
sharkspeare <jack04309487@gmail.com>
2026-04-26 00:13:09 +0800

    rafactor: 為每種資料個別構建類別，使用Entry作為抽象基底

--- models/entry.py
+++ models/entry.py
@@ -1,12 +1,130 @@
 import datetime as dt
 from models.types import EntryRaw
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
+@dataclass
+class Entry(ABC):
+    start: dt.datetime
+    end: dt.datetime
 
-class Entry:
-    def __init__(self, data:EntryRaw):
-        self.start = dt.datetime.fromisoformat(data["StartTime"])
-        self.end = dt.datetime.fromisoformat(data["EndTime"])
+    @classmethod
+    def _base_args(cls, data:EntryRaw):
+        return {
+            "start": data["StartTime"],
+            "end": data["EndTime"]
+        }
+    
+    @classmethod
+    @abstractmethod
+    def from_dict(cls, data:EntryRaw): ...
+    
 
-        for key, value in data.items():
-            if key not in ("StartTime", "EndTime"):
-                setattr(self, key, value)
+@dataclass
+class Temperature(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["Temperature"]
+        )
+
+@dataclass
+class Temperature(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["Temperature"]
+        )
+    
+@dataclass
+class Rain(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["ProbabilityOfPrecipitation"]
+        )
+    
+@dataclass
+class Direction(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["WindDirection"]
+        )
+    
+@dataclass
+class Speed(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["WindSpeed"]
+        )
+    
+@dataclass
+class UV(Entry):
+    index: str
+    level: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            index=data["UVIndex"],
+            level=data["UVExposureLevel"]
+        )
+    
+@dataclass
+class Weather(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["Weather"]
+        )
+    
+@dataclass
+class Desc(Entry):
+    value: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            value=data["WeatherDescription"]
+        )
+    
+@dataclass
+class Sun(Entry):
+    date: str
+    rise: str
+    tansit_time: str
+    transit_alt: str
+    set: str
+
+    @classmethod
+    def from_dict(cls, data:EntryRaw):
+        return cls(
+            **cls._base_args(data),
+            date=data["Date"],
+            rise=data["SunRiseTime"],
+            transit_time=data["SunTransitTime"],
+            transit_alt=data["SunTransitAlt"],
+            set=data["SunSetTime"]
+        )
